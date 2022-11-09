12db2b0aac90092c6b175bff0720fcb0a04d63d7
PinochetLab <78642617+PinochetLab@users.noreply.github.com>
2022-11-09 22:10:42 +0300

    Update schema.ts

--- frontend/src/api/schema.ts
+++ frontend/src/api/schema.ts
@@ -1,11 +1,12 @@
 import { axios } from '../axios';
 
 
-export const getAvailable = async (id: Number | undefined): Promise<string> => {
+export const getAvailable = async (id?: number): Promise<string> => {
     try {
         if (id === undefined) {
             return '';
         }
+
         const { data } = await axios.get<string>('/script/body', { params: { id } });
         return data;
     } catch (e) {
