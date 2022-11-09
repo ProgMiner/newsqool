b6d42d955d09999bdd716d00212558a256098aac
Eridan Domoratskiy <eridan200@mail.ru>
2022-11-09 17:42:37 +0300

    Fix linter issues and install packages

--- frontend/src/api/schema.ts
+++ frontend/src/api/schema.ts
@@ -3,13 +3,10 @@ import { axios } from '../axios';
 
 export const getAvailable = async (id: Number): Promise<string> => {
     try {
-        const { data } = await axios.get<string>('/script/body?id=' + id);
+        const { data } = await axios.get<string>('/script/body', { params: { id } });
         return data;
     } catch (e) {
         throw e;
         // TODO
     }
-
-    // TODO bad type
-    throw new Error();
 };
--- frontend/src/components/Schema/Schema.tsx
+++ frontend/src/components/Schema/Schema.tsx
@@ -10,17 +10,18 @@ import './Schema.css';
 
 export interface SchemaProps {
     className?: string;
-    currentSchemaId: Number;
+    currentSchemaId: number;
 }
 
 const cnSchema = cn('Schema');
 
 export const Schema: React.FC<SchemaProps> = ({ className, currentSchemaId }) => {
-    // const { schema } = useSchema(currentSchemaId);
-    const { schema } = useSchema(7);
+    const { schema } = useSchema(currentSchemaId);
+
     return (
         <div className={cnSchema(null, [className])}>
-            <LightAsync language='sql' style={docco} wrapLongLines>
+            <LightAsync language='sql' style={docco} wrapLongLines
+                        PreTag="div" codeTagProps={{ className: cnSchema('Panel') }}>
                 {schema ?? ''}
             </LightAsync>
         </div>
